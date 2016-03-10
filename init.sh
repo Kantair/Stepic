0906db8e9d63be4abc1c6ee437c61215ef24cc39
Kantair <tkachevanse@gmail.com>
2016-03-10 07:50:55 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,8 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo apt-get update
+sudo apt-get install nginx
+cd web
+mkdir -p public/img public/css public/js uploads etc
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-sudo gunicorn -c /etc/gunicorn.d/hello.py hello:application
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
